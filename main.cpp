1f06a6c93b176c37ec561f16d2d88c59e95d43ad
Loma <lomakim@gmail.com>
2026-05-21 18:59:33 -0700

    Update

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// COMSC210 | Final Exam 2 | Loma Kim
+#include <iostream>
+using namespace std;
+
+struct Node {
+    string name, order;
+    Node *next;
+};
+
+int main() {
+    Node *head = nullptr;
+
+    return 0;
+}
