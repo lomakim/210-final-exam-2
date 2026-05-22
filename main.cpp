0b4ce25d7004f1d55f12138eac5b33aab7b3131d
Loma <lomakim@gmail.com>
2026-05-21 19:14:37 -0700

    Update

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 // COMSC210 | Final Exam 2 | Loma Kim
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 struct Node {
@@ -8,7 +9,14 @@ struct Node {
 };
 
 int main() {
+    ifstream fin;
+    const int N_SIZE = 50, C_SIZE = 15;
+    string names[N_SIZE];
+    string coffees[C_SIZE];
     Node *head = nullptr;
 
+    fin.open("names.txt");
+    while(fin)
+    
     return 0;
 }
