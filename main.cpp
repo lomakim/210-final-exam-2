fa879823bddc855264207b156d4567060f26b94a
Loma <lomakim@gmail.com>
2026-05-21 19:23:50 -0700

    Milestone 1

--- main.cpp
+++ main.cpp
@@ -11,12 +11,28 @@ struct Node {
 int main() {
     ifstream fin;
     const int N_SIZE = 50, C_SIZE = 15;
+    string tempS;
     string names[N_SIZE];
     string coffees[C_SIZE];
     Node *head = nullptr;
 
+    //READ NAMES INTO ARRAY FROM FILE
     fin.open("names.txt");
-    while(fin)
-    
+    for (int i = 0; i < N_SIZE; i++) {
+        getline(fin, tempS);
+        names[i] = tempS;
+        cout << names[i] << endl;
+    }
+    fin.close();
+
+    //READ COFFEE ORDERS INTO ARRAY FROM FILE
+    fin.open("coffee-orders.txt");
+    for (int i = 0; i < C_SIZE; i++) {
+        getline(fin, tempS);
+        coffees[i] = tempS;
+        cout << coffees[i] << endl;
+    }
+    fin.close();
+
     return 0;
 }
